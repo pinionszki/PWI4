06401350905059fb80bc192c20d4409fca15af9d
pinionszki <63244802+pinionszki@users.noreply.github.com>
2021-03-29 15:25:52 +0200

    testing

---
+++ scripts.js
@@ -0,0 +1,95 @@
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+var firstPlayer = true;
+var singlePlayer = true;
+
+var handler = function(val) {
+	singlePlayer = val;
+}
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+
+function turnClick(square) {
+	if (typeof origBoard[square.target.id] == 'number') {
+		if (!singlePlayer){
+			currentPlayer = firstPlayer?huPlayer:aiPlayer
+			turn(square.target.id, currentPlayer)
+			firstPlayer = !firstPlayer
+		}
+		else{
+			turn(square.target.id, huPlayer)
+		}
+		if (!checkTie() && singlePlayer) turn(bestSpot(), aiPlayer);
+	}
+}
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+		for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	whoWon(gameWon.player == huPlayer ? "Wygrał O" : "Wygrał X");
+}
+
+function whoWon(who) {
+	document.querySelector(".endgame").style.display = "inline-block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		whoWon("Remis!")
+		return true;
+	}
+	return false;
+}
